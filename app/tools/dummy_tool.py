e17e27c854f2ede471ca6d1eafd8718ce7e3f7a4
Pablo <polo3ies@gmail.com>
2019-11-14 18:40:39 +0100

    Dummy tool is a class

--- app/tools/dummy_tool.py
+++ app/tools/dummy_tool.py
@@ -1,6 +1,10 @@
-def evaluate(screenshot):
-	return 1
+class DummyTool:
+	def __init__(self):
+		pass
+
+	def evaluate(self, screenshot):
+		return 0
 
 
-def act(screenshot):
-	return 'click', (100, 100)
+	def act(self, screenshot):
+		return 'wait', 1
